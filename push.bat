bad73ce487f69129c2fa25e554042e704698b4c1
aiwithr <raqueebh@yahoo.com>
2021-12-14 23:32:35 +0600

    Auto commit from VScode

--- push.bat
+++ push.bat
@@ -8,6 +8,6 @@ git commit -m "Auto commit from VScode"
 :: git remote add origin git@aiwithr:aiwithr/aiwithr.github.io.git
 :: git remote add origin https://aiwithr@github.com/aiwithr.github.io.git
 git remote add origin https://github.com/aiwithr/aiwithr.github.io.git
-git push origin master --force
-:: git push -u origin master
+:: git push origin master --force
+git push -u origin master
 :: rmdir .git /s /q
